7c2fe1522ee200db4906d22cf5ae9a6b04be6244
atharva <atharvarana29@gmail.com>
2026-02-26 00:37:37 +0530

    added the signup and signin feature

--- oops_proj.py
+++ oops_proj.py
@@ -20,15 +20,15 @@ class chatbook:
         if user_input == "1":
             self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
-            pass
+            self.writing_post()
         elif user_input == "4":
-            pass
+            self.message_Friend()
         else:
             exit()
 
-    def signup(self):
+    def signup(self): # self refers to this newly created instance  
         email = input("Enter your email here: ")
         pwd = input("enter your password  here : ")
         self.username = email
@@ -36,6 +36,29 @@ class chatbook:
         print("you have signed up successfully")
         print("\n")
         self.menu() # ye menu ka line add karna i.e menu method call karna hi show karta hai sabse bada advantage of oops concept and modularity 
+
+    def signin(self):
+        # if self.signup == True:  this is not the correct way to check the sign up
+        if self.username=='' and self.password == '' :
+            print("we donot have any user name and password from your side .\n so please sign up first by pressing 1 in the main menu")
+            self.menu()
+        else: # i.e when our user has already signed up 
+            uname = input("greate to see you back here . please enter your email/username here : ")
+            pwd_1 = input("please enter your password here")
+            self.username = uname
+            self.password = pwd_1
+
+            # now we need to check that if the username and the password given matches to the one given during signin 
+            if uname == self.username and pwd_1 == self.password:    
+                print("you have signed in successfully\n welcome to you your account ")
+                print("\n")
+                # now initially we had set self.logged in as false , but now the user has signed in successfully 
+                # so we can turn off that 
+                self.loggedin = True
+            else:
+                print("\n please provide the correct credentials")
+        print("\n")
+        self.menu()
 
 
 
