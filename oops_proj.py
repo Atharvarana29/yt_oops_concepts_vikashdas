fcfb675099cc71c5b4c442e3ef9cba498b967d2e
atharva <atharvarana29@gmail.com>
2026-02-25 17:36:33 +0530

    added signup logic

--- oops_proj.py
+++ oops_proj.py
@@ -1,9 +1,10 @@
 class chatbook:
-    def __init__(self):
+    # initialized the constructor 
+    def __init__(self): # self -> refers to the newly created instance 
         self.username = ''
         self.password = ''
         self.loggedin = False # initially
-        self.menu() # method nniche alag sae bhi bana kae upar yaha par attribute mae bhi call kar sakte hai 
+        self.menu() # method nniche alag sae bhi bana kae upar yaha par attribute kae taur par bhi call kar sakte hai . jo bhi method ko call karna hai usko yaha likh dena hai 
     # humare constructor kae andar hi yae logic tha ki menu ko call karna hai  
 
     # defining the main menu method 
@@ -17,7 +18,7 @@ class chatbook:
                            ''')
         
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
             pass
         elif user_input == "3":
@@ -26,6 +27,16 @@ class chatbook:
             pass
         else:
             exit()
+
+    def signup(self):
+        email = input("Enter your email here: ")
+        pwd = input("enter your password  here : ")
+        self.username = email
+        self.password = pwd
+        print("you have signed up successfully")
+        print("\n")
+        self.menu() # ye menu ka line add karna i.e menu method call karna hi show karta hai sabse bada advantage of oops concept and modularity 
+
 
 
 obj = chatbook()
